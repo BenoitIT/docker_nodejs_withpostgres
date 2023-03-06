a41e841fcd723077de56c6754ba39caeb87de043
benoitIT <benoitnda35@gmail.com>
2023-03-06 12:59:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const {createPost,viewPost,readOnePost} = require('./controllers/posts');
+const {createUser,viewUser,readOne,deleteUser,updateUser} = require('./controllers/user');
+const {connectDb}=require('./models/index')
+const app = express();
+app.use(express.json());
+app.post('/api/users/newuser',createUser);
+app.get('/api/users/all',viewUser);
+app.get('/api/users/:id',readOne);
+app.post('/api/posts/newpost',createPost);
+app.get('/api/post/all',viewPost);
+app.get('/api/post/:id',readOnePost);
+app.delete('/api/users/:id',deleteUser);
+app.patch('/api/users/:id',updateUser);
+connectDb();
+app.listen(3000,()=>console.log('listening on port'));
